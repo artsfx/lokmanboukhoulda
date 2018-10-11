ae9451e0253e59db6725be76d3fe53fbab2daa4e
artsfx <boukhoulda.lokman@gmail.com>
2018-10-10 23:09:40 -0400

    Updated my portfolio

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-window.onscroll = function() {
-console.log("aboutme:"+document.querySelector("#aboutme").getBoundingClientRect().top+" , scrollY:"+window.scrollY);
-	}
 function aboutme(event) {
-		let jumbotron = document.querySelector(".jumbotron-container");
-		let aboutme = document.querySelector("#aboutme");
-		let aboutmePos = aboutme.getBoundingClientRect().top;
-		jumbotron.style.transitionProperty = "all";
-		window.scrollBy(0, aboutmePos);
+    let jumbotron = document.querySelector(".jumbotron-container");
+    let aboutme = document.querySelector("#aboutme");
+    let aboutmePos = aboutme.getBoundingClientRect().top;
+    jumbotron.style.transitionProperty = "all";
+    window.scrollBy(0, aboutmePos);
 }
 
 
 
 function portfolio(event) {
-	let portfolio = document.querySelector("")
+    // let jumbotron = document.querySelector(".jumbotron-container");
+    let portfolio = document.querySelector(".portfolio-container");
+    let portfolioPos = portfolio.getBoundingClientRect().top;
+    // jumbotron.style.transitionProperty = "all";
+    window.scrollBy(0, portfolioPos);
+}
+
+function contact(event) {
+    // let jumbotron = document.querySelector(".jumbotron-container");
+    let contact = document.querySelector(".contact-container");
+    let contactPos = contact.getBoundingClientRect().top;
+    // jumbotron.style.transitionProperty = "all";
+    window.scrollBy(0, contactPos);
 }
